a933cea1f5355f82d01d21039799c9dbc1cbd5ea
michmakarov <michmakarov@gmail.com>
2020-05-22 20:21:43 +0300

    improved names of global variables

--- help.go
+++ help.go
@@ -11,6 +11,6 @@ Then it shows them in one or another way.
 If the prog is run and number of params less than two it prints this text and ends its work.
 Otherwise it expects next params in order of writing:
 1. An dotted address of destination of a IP packet . In other word, it is a net interface to which the packet was sent.
-For example, 127.0.0.1 or 192.168.1.44
+For example, 127.1.1.123 or 192.168.1.44
 2. A decimal number of IP protocol. For example: 1 for ICMP; 6 - TCP; 17 - UDP.
 `
--- mn.go
+++ mn.go
@@ -15,8 +15,8 @@ var (
 	git_commit_1 = "no data of building"
 	ipConn       *net.IPConn
 	lAddr        *net.IPAddr
-	nt           string //= "ip:1"
-	intrf        string //= "192.168.1.44"
+	proto        string //1 for ICMP
+	destAddr     string //destination address, like "192.168.1.44" or "127.12.123.1"
 )
 
 //Exit codes
@@ -27,12 +27,16 @@ func main() {
 	var iterC int
 	var buff = make([]byte, 60000)
 	var n int
-	fmt.Printf("200311_ipsniffer %v; pid=%v", git_commit_1, os.Getpid())
+	var nt string
+	fmt.Printf("200311_ipsniffer %v; pid=%v\n", git_commit_1, os.Getpid())
 	if len(os.Args) < 3 {
 		log.Println(help)
 		return
 	}
-	if lAddr, err = net.ResolveIPAddr(nt, intrf); err != nil {
+	destAddr = os.Args[1]
+	proto = os.Args[2]
+	nt = "ip:" + proto
+	if lAddr, err = net.ResolveIPAddr(nt, destAddr); err != nil {
 		err = fmt.Errorf("200311_ipsniffer: net.ResolveIPAddr err=%v", err.Error())
 		log.Printf("%v\n", err.Error())
 		os.Exit(110)
